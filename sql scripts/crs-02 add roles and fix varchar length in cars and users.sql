269e4f0a21336ab85a5be976e25b2bb0c428ac86
PPUARAT <mr.pangseen@gmail.com>
2020-03-02 14:43:10 +1300

    Fix typo and correct sql statements

--- sql scripts/crs-02 add roles and fix varchar length in cars and users.sql
+++ sql scripts/crs-02 add roles and fix varchar length in cars and users.sql
@@ -1,5 +1,5 @@
 -- add data into roles
 INSERT INTO roles (name, description, created_at, is_active) 
-VALUES('Admin', 'Super user', now(), 1),
-('Staff', 'Able to perform all staff tasks', now(), 1),
-('Customer', 'Basic functions', now(), 1);
+VALUES('Admin', 'Super user', current_timestamp(), 1),
+('Staff', 'Able to perform all staff tasks', current_timestamp(), 1),
+('Customer', 'Customer of this system. Basic functions', current_timestamp(), 1);
