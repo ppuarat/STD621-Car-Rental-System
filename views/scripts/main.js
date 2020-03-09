7f8b2d384a0a096216f3426a2b7f310c05f07f56
ppuarat <boomer_saw@hotmail.com>
2020-03-09 16:22:09 +1300

    Added :pencil:comments and :tada:a new request function

--- views/scripts/main.js
+++ views/scripts/main.js
@@ -1,5 +1,5 @@
 "use strict";
-
+//Global car on index.php
 var CAR;
 function rentModal(car) {
     CAR = car;
@@ -14,16 +14,16 @@ function rentModal(car) {
     $('#rentModal').modal('toggle');
 
 }
-
+//datepicker on rent modal
 $('#datepicker').datepicker({
     startDate: '0d',
     todayBtn: "linked"
 });
-
+//Recalculate the total price when user select a new date
 function dateChanged() {
     $("#total").html("Rent this car for: " + findDiffDays() + " days. The total cost is " + totalPrice());
 }
-
+//index.php 
 function submitRent() {
     //Prepare Data
     let data = {
@@ -58,4 +58,33 @@ function findDiffDays() {
     let diffTime = Math.abs(endDate - startDate);
     let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) + 1;
     return diffDays;
+}
+//For Request.php
+function approveRequest(rentalId, isApprove) {
+    if(isApprove){
+        console.log("Approved");
+    }else{
+        console.log("Rejected");
+    }
+    //Prepare Data
+    let data = {
+        functionName: "create",
+        car: CAR,
+        fromDate: $('#start').val(),
+        toDate: $('#end').val(),
+        total: totalPrice()
+    };
+    var settings = {
+        "url": "/car-rental-system/controllers/RentalController.php",
+        "method": "POST",
+        "timeout": 0,
+        "data": data
+    };
+    //Call Ajax
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        alert(response);
+        location.reload();
+    });
+
 }
