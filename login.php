0a3dc864d62ca04279196093c6d7f28f2e992a9b
psantander <psantander@users.noreply.github.com>
2020-04-03 10:39:35 +1300

    login_v3

--- login.php
+++ login.php
@@ -2,53 +2,43 @@
 
 session_start();
 
+}
+
 	include 'models/MySQLConnection.php';
+	include 'models/User.php';
+    include 'controllers/UserController.php';
+
     $conn_obj = new MySQLConnection;
     $conn = $conn_obj->getConnection();
 
+    $email = "";
+    $password = "";
+
+
     //Extract or grab the data from the form
     // and store those values in these two variables
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-		//Grab the form variables stored in the POST global variable i.e. the POST Request object
-		$email = $_POST['email'];
-		$password = $_POST['password'];
-        $table_name = 'users';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $userDao = new UserDao();
+    // username and password sent from form
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $email = $userController->find($email);
 
-        //write code here which adds them as a new record to my database
-        $sql_stmt = "SELECT * FROM " . $table_name . " WHERE
-            email = '" . $email . "' AND password = '" . $password . "' Limit 1";
-
-        //echo $sql_stmt; //Check if my statement is correct as per sql syntax
-        $result = $conn->query($sql_stmt);
-
-        if ($result->num_rows > 0) {
-            // get the first record from the results array
-           if($row = $result->fetch_array()){
-
-               // store the database values of this record into session
-               $_SESSION['id'] = $row['id'];
-               $_SESSION['first_name'] = $row['first_name'];
-               $_SESSION['last_name'] = $row['last_name'];
-               $_SESSION['email'] = $row['email'];
-               $_SESSION['password'] = $row['password'];
-
-
-               print_r($_SESSION); //prints all session variables
-
-
-               header("location: index.php");
-               exit;
-           }
-
+    // If user with such username does not exist or
+    // has different password
+    if (!$user || $user->getPassword() !== $password) {
+        $error = "Invalid credentials";
+    } else {
+        $_SESSION['user'] = serialize($user);
+        if ($user->getFk_role_id() == 1) {
+            header("location: admin.php");
+            exit;
+        } else {
+            header("location: index.php");
+            exit;
         }
-        else {
-            echo "Sorry, your email or password maybe incorrect";
-        }
-
-        //close Connection
-        $conn->close();
-
     }
+}
 ?>
 
 <!DOCTYPE html>
@@ -61,12 +51,12 @@ session_start();
     <?php include './views/navbar.php' ?>
 
     <div class="container">
-        <!-- Content here -->
-        <!-- page header -->
 
-
-            <div class = "d-flex justify-content-center border_si">
-                    <form method="POST" action="" style="padding : 15% 35% 20%" >
+                      <div class="logo" style="text-align: center;">
+                          <img src="views/img/cars/logo.jpg" alt="logo"/>
+                      </div>
+           // <div class = "d-flex justify-content-center border_si">
+                    <form method="POST" action="" style="padding : 0% 25% 15%" >
                     <div class="form-group">
                         <label for="email">Email address</label>
                         <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
