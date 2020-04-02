e191306e46ba1140be8727abbbe51f594864d0ef
psantander <psantander@users.noreply.github.com>
2020-04-03 01:02:02 +1300

    login_1trial

--- login.php
+++ login.php
@@ -1,3 +1,56 @@
+<?php
+
+session_start();
+
+	include 'models/MySQLConnection.php';
+    $conn_obj = new MySQLConnection;
+    $conn = $conn_obj->getConnection();
+
+    //Extract or grab the data from the form
+    // and store those values in these two variables
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+		//Grab the form variables stored in the POST global variable i.e. the POST Request object
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+        $table_name = 'users';
+
+        //write code here which adds them as a new record to my database
+        $sql_stmt = "SELECT * FROM " . $table_name . " WHERE
+            email = '" . $email . "' AND password = '" . $password . "' Limit 1";
+
+        //echo $sql_stmt; //Check if my statement is correct as per sql syntax
+        $result = $conn->query($sql_stmt);
+
+        if ($result->num_rows > 0) {
+            // get the first record from the results array
+           if($row = $result->fetch_array()){
+
+               // store the database values of this record into session
+               $_SESSION['id'] = $row['id'];
+               $_SESSION['first_name'] = $row['first_name'];
+               $_SESSION['last_name'] = $row['last_name'];
+               $_SESSION['email'] = $row['email'];
+               $_SESSION['password'] = $row['password'];
+
+
+               print_r($_SESSION); //prints all session variables
+
+
+               header("location: index.php");
+               exit;
+           }
+
+        }
+        else {
+            echo "Sorry, your email or password maybe incorrect";
+        }
+
+        //close Connection
+        $conn->close();
+
+    }
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -15,12 +68,12 @@
             <div class = "d-flex justify-content-center border_si">
                     <form method="POST" action="" style="padding : 15% 35% 20%" >
                     <div class="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                        <label for="email">Email address</label>
+                        <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"><br>
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" name "password" id="password" placeholder="Password"><br>
                         <div class = "container text-center">
                         <button type="submit" class="btn btn-primary">Login</button>
                         <button type="submit" class="btn btn-primary">Forgot password</button><br><br>
@@ -34,6 +87,4 @@
     <?php include './views/rentModal.php' ?>
 
 </body>
-<script src=""></script>
-
 </html>
--- signup_ajax.php
+++ signup_ajax.php
@@ -1,4 +1,3 @@
-
 <?php
 	include 'models/MySQLConnection.php';
     $conn_obj = new MySQLConnection;
